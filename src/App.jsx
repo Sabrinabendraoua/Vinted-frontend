b38ae353e6ee14a248be71c1225107679f86cada
sabrina bendraoua <sabbendra@gmail.com>
2023-05-03 17:28:13 +0200

    Header css

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 // Pages
 import Home from "./pages/Home";
-import Offer from "./pages/Offer";
+// import Offer from "./pages/Offer";
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/Offer" element={<Offer />} /> */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/Offer" element={<Offer />} /> */}
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
+      <section className="hero">
+        <div>
+          <p>Prêts à faire du tri dans vos placards ?</p>
+          <button>Commencer à vendre</button>
+        </div>
+      </section>
+      <section></section>
       <Link to="/offer">
         <button>Naviguer vers Offer</button>
       </Link>
