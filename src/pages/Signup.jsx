0c0065148d66005b1ba1de93304ecff89410f15d
sabrina bendraoua <sabbendra@gmail.com>
2023-05-04 18:25:29 +0200

    page Signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,16 +1,3 @@
-import { useEffect, useState, Link } from "react";
-import axios from "axios";
-
-const Form = () => {
-  return isLoading ? (
-    <p>Loading ...</p>
-  ) : (
-    <section>
-      <div>
-        <p>coucou</p>
-      </div>
-    </section>
-  );
-};
+const Form = () => {};
 
 export default Form;
